7f8d42c6afe222bd6874a6fff932c187293dd9fb
Arutemu64 <arutemu64@outlook.com>
2023-07-09 14:34:49 +0300

    Requests type-hinting

--- bot/db/requests.py
+++ bot/db/requests.py
@@ -1,23 +1,36 @@
+from typing import TypeVar, Generic, Type, List
+
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import declarative_base
+
+T = TypeVar("T")
+Base = declarative_base()
 
 
-class Requests(object):
+class Requests(Generic[T]):
+
+    type_model: Type[Base]
+    session: AsyncSession
+
+    def __init__(self, type_model: Type[Base], session: AsyncSession):
+        self.type_model = type_model
+        self.session = session
 
     @classmethod
-    async def get_one(cls, session: AsyncSession, whereclause):
+    async def get_one(cls, session: AsyncSession, whereclause) -> T:
         db_query = await session.execute(select(cls).where(whereclause))
         result = db_query.scalar_one_or_none()
         return result
 
     @classmethod
-    async def get_many(cls, session: AsyncSession, whereclause):
+    async def get_many(cls, session: AsyncSession, whereclause) -> List[T]:
         db_query = await session.execute(select(cls).where(whereclause))
         result = db_query.scalars().all()
         return result
 
     @classmethod
-    async def get_range(cls, session: AsyncSession, start: int, end: int, sort_by):
+    async def get_range(cls, session: AsyncSession, start: int, end: int, sort_by) -> List[T]:
         db_query = await session.execute(select(cls).order_by(sort_by).slice(start, end))
         return db_query.scalars().all()
 
