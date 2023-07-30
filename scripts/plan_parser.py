7cfc5adf618d097d52d2ec14cc721564e00651b1
Arutemu64 <arutemu64@outlook.com>
2023-07-30 11:10:09 +0300

    Improve plan_parser.py

--- scripts/plan_parser.py
+++ scripts/plan_parser.py
@@ -7,36 +7,17 @@ from sqlalchemy import text
 
 from scripts.config import DatabaseConfig
 from src.db.database import Database, create_async_engine, create_session_maker
+from src.db.models import Nomination, Participant
 
 path_to_plan = Path(__file__).with_name("technical_plan.csv")
 
 
-async def clean_db(db: Database):
-    print("Удаляем голоса...")
-    nominations = await db.nomination.get_many(True, limit=1000)
-    for nomination in nominations:
-        await db.session.delete(nomination)
-    await db.session.execute(text("ALTER SEQUENCE votes_id_seq RESTART WITH 1"))
-    await db.session.commit()
-
-    print("Удаляем события...")
+async def clean_schedule(db: Database):
+    print("Очищаем расписание...")
     events = await db.event.get_many(True, limit=1000)
     for event in events:
         await db.session.delete(event)
     await db.session.execute(text("ALTER SEQUENCE schedule_id_seq RESTART WITH 1"))
-    await db.session.commit()
-
-    print("Удаляем участников...")
-    participants = await db.participant.get_many(True, limit=1000)
-    for participant in participants:
-        await db.session.delete(participant)
-    await db.session.execute(text("ALTER SEQUENCE participants_id_seq RESTART WITH 1"))
-    await db.session.commit()
-
-    print("Удаляем номинации...")
-    nominations = await db.nomination.get_many(True, limit=1000)
-    for nomination in nominations:
-        await db.session.delete(nomination)
     await db.session.commit()
 
 
@@ -44,37 +25,68 @@ async def parse_plan(db: Database):
     df = pd.read_csv(path_to_plan.absolute())
     df["next_code"] = df["code"].shift(-1)
     df.replace({np.nan: None}, inplace=True)
+    participants_count = 0
+    nominations_count = 0
     for index, row in df.iterrows():
         if row["code"] is not None:  # participant
-            participant = await db.participant.new(
-                title=row["voting_title"], nomination_id=row["code"]
+            participant = await db.participant.get_by_where(
+                Participant.title == row["voting_title"]
             )
-            await db.session.flush([participant])
+            if participant:
+                print(f"Участник {participant.title[0:40]}... уже добавлен ранее")
+                pass
+            else:
+                print(f"Добавляем нового участника {row['voting_title'][0:40]}... в БД")
+                participant = await db.participant.new(
+                    title=row["voting_title"], nomination_id=row["code"]
+                )
+                await db.session.flush([participant])
+                participants_count += 1
             event = await db.event.new(participant_id=participant.id)
             await db.session.flush([event])
         elif row["code"] is None and row["next_code"] is not None:  # nomination
-            nomination = await db.nomination.new(
-                nomination_id=row["next_code"], title=row["info"]
+            nomination = await db.nomination.get_by_where(
+                Nomination.id == row["next_code"]
             )
-            await db.session.flush([nomination])
+            if nomination:
+                print(f"Номинация {row['next_code']} уже добавлена ранее")
+                pass
+            else:
+                print(f"Добавляем новую номинацию {row['info']}...")
+                while True:
+                    choice = input(
+                        "Разрешить голосование за эту номинацию? Y - да, N - нет\n"
+                    )
+                    if choice.lower() == "y":
+                        votable = True
+                        break
+                    elif choice.lower() == "n":
+                        votable = False
+                        break
+                nomination = await db.nomination.new(
+                    nomination_id=row["next_code"], title=row["info"], votable=votable
+                )
+                await db.session.flush([nomination])
+                nominations_count += 1
         elif row["code"] is None and row["next_code"] is None:  # schedule entry
             event = await db.event.new(title=row["info"])
             await db.session.flush([event])
     await db.session.commit()
+    return participants_count, nominations_count
 
 
 async def main(session_pool):
     async with session_pool() as session:
         db = Database(session)
-        print("Очищаем БД...")
-        await clean_db(db)
-        print("БД очищена! Переходим к парсингу плана...")
-        await parse_plan(db)
-        participants_count = await db.participant.count(True)
+        print("Очищаем расписание...")
+        await clean_schedule(db)
+        print("Расписание очищено! Переходим к парсингу плана...")
+        participants_count, nominations_count = await parse_plan(db)
         events_count = await db.event.count(True)
-        nominations_count = await db.nomination.count(True)
-        print(f"Парсинг завершён! Было добавлено {str(nominations_count)} номинации, "
-              f"{str(participants_count)} участников и {str(events_count)} событий!")
+        print(
+            f"Парсинг завершён! Было добавлено {str(nominations_count)} новых номинаций, "
+            f"{str(participants_count)} новых участников и {str(events_count)} событий."
+        )
     return
 
 
