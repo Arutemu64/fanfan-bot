913f2b13abee542762996087ca2277f787b2741a
Arutemu64 <arutemu64@outlook.com>
2023-08-10 13:35:28 +0300

    Show lock icon when voting is disabled

--- src/bot/dialogs/main/main.py
+++ src/bot/dialogs/main/main.py
@@ -1,10 +1,11 @@
 import math
 import random
 
+from aiogram import F
 from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager, Window
 from aiogram_dialog.widgets.kbd import Button, Row, Start, SwitchTo, Url
-from aiogram_dialog.widgets.text import Const, Format
+from aiogram_dialog.widgets.text import Case, Const, Format
 
 from src.bot.dialogs import states
 from src.bot.structures import Role
@@ -16,18 +17,21 @@ from src.db.models import Event, User
 per_page = conf.bot.events_per_page
 
 
-async def getter(dialog_manager: DialogManager, user: User, **kwargs):
+async def getter(dialog_manager: DialogManager, user: User, db: Database, **kwargs):
     name = dialog_manager.event.from_user.first_name
     is_helper = user.role == Role.HELPER
     is_org = user.role == Role.ORG
     if is_org:
         is_helper = True
     random_quote = random.choice(strings.quotes)
+    voting_enabled = (await db.settings.get_by_where(True)).voting_enabled
+    dialog_manager.dialog_data["voting_enabled"] = voting_enabled
     return {
         "name": name,
         "is_helper": is_helper,
         "is_org": is_org,
         "random_quote": random_quote,
+        "voting_enabled": voting_enabled,
     }
 
 
@@ -42,6 +46,14 @@ async def open_schedule(
         current_event_id = 1
     current_page = math.floor((current_event_id - 1) / per_page)
     await manager.start(state=states.SCHEDULE.MAIN, data={"current_page": current_page})
+
+
+async def open_voting(callback: CallbackQuery, button: Button, manager: DialogManager):
+    voting_enabled = manager.dialog_data.get("voting_enabled")
+    if voting_enabled:
+        await manager.start(state=states.VOTING.NOMINATIONS)
+    else:
+        await callback.answer(strings.errors.voting_disabled, show_alert=True)
 
 
 main = Window(
@@ -66,8 +78,16 @@ main = Window(
             state=states.MAIN.ACTIVITIES,
         ),
     ),
-    Start(
-        text=Const(strings.buttons.voting), state=states.VOTING.NOMINATIONS, id="voting"
+    Button(
+        text=Case(
+            texts={
+                True: Const(strings.buttons.voting),
+                False: Const(strings.buttons.voting_locked),
+            },
+            selector=F["voting_enabled"],
+        ),
+        id="voting",
+        on_click=open_voting,
     ),
     Row(
         Start(
