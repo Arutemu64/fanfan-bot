c8fe8da6da5cf22714220a746f6eade55e055dd3
Arutemu64 <arutemu64@outlook.com>
2023-09-03 17:30:00 +0300

    Stats for orgs

--- src/bot/dialogs/org/main.py
+++ src/bot/dialogs/org/main.py
@@ -2,16 +2,88 @@ from aiogram import F
 from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager, Window
 from aiogram_dialog.widgets.kbd import Button, Cancel, SwitchTo
-from aiogram_dialog.widgets.text import Case, Const
+from aiogram_dialog.widgets.text import Case, Const, Jinja
 
 from src.bot.dialogs import states
+from src.bot.structures import UserRole
 from src.bot.ui import strings
+from src.db import Database
+from src.db.models import Event, User
 from src.redis.global_settings import GlobalSettings
 
+# fmt: off
+StatsTemplate = Jinja(  # noqa
+    "{% for info in bot_info %}"
+        "{% if info.name %}"
+            """<b>{{ info.name }}</b>: {{ info.value }}\n"""
+        "{% else %}"
+            "\n\n"
+        "{% endif %}"
+        "{% for sub in info.subs %}"
+            """  {{ "├─" if not loop.last else "└─" }}<b>{{ sub.name }}</b>: {{ sub.value }}\n"""  # noqa: E501
+        "{% endfor %}"
+    """{% endfor %}"""
+)
+# fmt: on
 
-async def getter(settings: GlobalSettings, **kwargs):
+
+async def org_menu_getter(db: Database, settings: GlobalSettings, **kwargs):
     voting_enabled = await settings.voting_enabled.get()
-    return {"voting_enabled": voting_enabled}
+    return {
+        "voting_enabled": voting_enabled,
+        "bot_info": (
+            {
+                "name": "🎫 Билетов",
+                "value": await db.ticket.get_count(),
+            },
+            {
+                "name": "👥 Пользователей",
+                "value": await db.user.get_count(),
+                "subs": (
+                    {
+                        "name": "👀 Зрителей",
+                        "value": await db.user.get_count(User.role == UserRole.VISITOR),
+                    },
+                    {
+                        "name": "📣 Волонтёров",
+                        "value": await db.user.get_count(User.role == UserRole.HELPER),
+                    },
+                    {
+                        "name": "🧑‍🍳 Организаторов",
+                        "value": await db.user.get_count(User.role == UserRole.ORG),
+                    },
+                ),
+            },
+            {
+                "name": "💃 Выступлений",
+                "value": await db.event.get_count(),
+                "subs": (
+                    {
+                        "name": "🫣 Пропущено",
+                        "value": await db.event.get_count(Event.skip.is_(True)),
+                    },
+                ),
+            },
+            {
+                "name": "🧑‍🎤 Участников",
+                "value": await db.participant.get_count(),
+            },
+            {
+                "name": "🥈 Номинаций",
+                "value": await db.nomination.get_count(),
+                "subs": (
+                    {
+                        "name": "🥇 С голосованием",
+                        "value": await db.nomination.get_count(),
+                    },
+                ),
+            },
+            {
+                "name": "🗣️ Голосов",
+                "value": await db.vote.get_count(),
+            },
+        ),
+    }
 
 
 async def switch_voting(
@@ -22,8 +94,9 @@ async def switch_voting(
 
 
 org_menu = Window(
-    Const("<b>🔧 Меню организатора</b>"),
-    Const(" "),
+    Const("<b>🔧 Меню организатора</b>\n"),
+    Const("🔢 <b>Статистика использования бота:</b>\n"),
+    StatsTemplate,
     SwitchTo(
         state=states.ORG.ASK_USERNAME,
         id="edit_user",
@@ -47,5 +120,5 @@ org_menu = Window(
     ),
     Cancel(Const(strings.buttons.back)),
     state=states.ORG.MAIN,
-    getter=getter,
+    getter=org_menu_getter,
 )
