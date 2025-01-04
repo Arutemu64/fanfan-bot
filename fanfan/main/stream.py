76174932086807d13754a093052ee9c8f1d52baa
Arutemu64 <arutemu64@outlook.com>
2025-01-05 00:54:06 +0300

    Add stream workers

--- fanfan/main/stream.py
+++ fanfan/main/stream.py
@@ -40,7 +40,13 @@ def create_app() -> AsgiFastStream:
 def main():
     if sys.platform == "win32":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    uvicorn.run(create_app(), host="0.0.0.0", port=8000)  # noqa: S104
+    uvicorn.run(
+        "fanfan.main.stream:create_app",
+        factory=True,
+        workers=3,
+        host="0.0.0.0",  # noqa: S104
+        port=8000,
+    )
 
 
 if __name__ == "__main__":
